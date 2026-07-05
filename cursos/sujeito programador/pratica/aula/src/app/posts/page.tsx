b02da0acbade439368fa32aa769e2bd90488101d
Rafael Ott <157972006+rafaelottdev@users.noreply.github.com>
2026-07-04 23:51:54 -0300

    resume 2

--- cursos/sujeito programador/pratica/aula/src/app/posts/page.tsx
+++ cursos/sujeito programador/pratica/aula/src/app/posts/page.tsx
@@ -1,6 +1,7 @@
 import Button from "@/components/Button"
+import Link from "next/link"
 
-interface PostProps {
+export interface PostProps {
     id: number,
     title: string,
     body: string,
@@ -15,17 +16,45 @@ export default async function PostsPage() {
     const response = await fetch("https://dummyjson.com/posts")
     const data: ResponseProps = await response.json()
 
-    console.log(data)
+
+    async function handleFetchPosts() {
+        "use server"
+
+        const response = await fetch("https://dummyjson.com/posts")
+        const data: ResponseProps = await response.json()
+
+        console.log(data.posts)
+    }
+
+    async function handleSearchUsers(formData: FormData) {
+        "use server"
+
+        const userId = formData.get("userId")
+
+        const response = await fetch(`https://dummyjson.com/posts/user/${userId}`)
+        const data: ResponseProps = await response.json()
+
+        console.log(data)
+    }
 
     return (
         <div>
             <h1 className="text-center mt-5 mb-2 font-bold text-3xl">Todos os posts</h1>
+
+            <button onClick={handleFetchPosts}>Buscar Posts</button>
+
+            <form className="flex gap-2 my-4" action={handleSearchUsers}>
+                <input type="text" placeholder="id do usuario" className="border border-gray-200 p-2" name="userId" />
+                <button className="bg-blue-500 text-white p-2" type="submit">Buscar usuario</button>
+            </form>
 
             <div className="flex flex-col gap-4 mx-2">
                 {data.posts.map((post) => (
                     <div key={post.id} className="bg-gray-200 p-4 rounded-md">
                         <h2 className="font-bold">{post.title}</h2>
                         <p>{post.body}</p>
+                        
+                        <Link href={`/posts/${post.id}`}>Acessar detalhes</Link>
                     </div>
                 ))}
             </div>
